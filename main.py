8eda8ccdfce5062b9d46059639a5333bc3780565
Dmytro Sh <dmytro.shlapak@outlook.com>
2019-01-30 17:10:21 +0200

    test branche

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-ch = input('Please some test')
+def print_test():
+    ch = input('Please some test')
+    if len(ch)>0:
+        check = True
+    else:
+        check = False
+    if check:
+        print('OK')
+    return 0
 
-if len(ch)>0:
-    check = True
-else:
-    check = False
-
-if check:
-    print('OK')
 
